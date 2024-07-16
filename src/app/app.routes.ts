ec88b3ec9cac803eb0831a67d4338391b1bea47c
vinod <vinodkus@gmail.com>
2024-07-16 21:28:40 +0530

    angualr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { LayoutComponent } from './Pages/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'home',component:HomeComponent
+            }
+        ]
+        //component:HomeComponent
+    }
+];
