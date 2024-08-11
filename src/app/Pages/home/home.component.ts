3a81ec390c3b5484adbc0e910efeabc64b3184a4
vinod <vinodkus@gmail.com>
2024-08-11 23:36:13 +0530

    f

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -15,12 +15,17 @@ profObjLst:any=[{
   skill:"Bridal MehndiBridal & Traditional Mehndi Expert",
   location:"123 Main Street, Anytown",
   experience:"10 years",
+  contact:"98765223345",
+  about:"John is a highly skilled mehndi artist specializing in intricate bridal designs and traditional patterns. With over 10 years of experience, he has been a part of many memorable events and has a keen eye for detail."
 },
 {
   name:"Vijay",
   skill:"Contemporary & Festive Mehndi Artist",
   location:"456 Elm Street, Anycity",
   experience:"8 years",
+  contact:"9876749460",
+  about:"John is a highly skilled mehndi artist specializing in intricate bridal designs and traditional patterns. With over 10 years of experience, he has been a part of many memorable events and has a keen eye for detail."
+
 }
 ,
 {
@@ -28,6 +33,8 @@ profObjLst:any=[{
   skill:"Palm Mehndi Artist",
   location:"123 Elm Street, Anycity",
   experience:"10 years",
+  contact:"9987643210",
+  about:"John is a highly skilled mehndi artist specializing in intricate bridal designs and traditional patterns. With over 10 years of experience, he has been a part of many memorable events and has a keen eye for detail."
 }
 ]
   constructor() {
